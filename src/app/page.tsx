b9e91721ae07a7ebf9ff5d17e62d7ed613d2e49e
Collin Stasiak <collinstasiak@gmail.com>
2024-05-30 11:48:44 -0400

    only show pictures on signed in users

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,32 @@
 import { db } from "~/server/db";
+import { SignedOut, SignedIn } from '@clerk/nextjs';
 
 //Tell next not to cache because database will change
 export const dynamic = "force-dynamic";
 
-
-export default async function HomePage() {
+async function Images() {
     const images = await db.query.images.findMany({ orderBy: (model, { desc }) => desc(model.id), });
     return (
-        <main>
-            <div className="flex flex-wrap gap-4">
-                {[...images, ...images, ...images].map((image) => (
-                    <div key={image.id} className="w-48 flex flex-col">
-                        <img src={image.url} alt="image" />
-                        <div>{image.name}</div>
-                    </div>
-                ))
-                }
+    <div className="flex flex-wrap gap-4">
+        {[...images, ...images, ...images].map((image) => (
+            <div key={image.id} className="w-48 flex flex-col">
+                <img src={image.url} alt="image" />
+                <div>{image.name}</div>
             </div>
+        ))
+        }
+    </div>
+    )
+}
+
+
+export default async function HomePage() {
+    return (
+        <main>
+            <SignedOut>
+                <div className="w-full h-full text-2xl text-center">Please Sign in Above</div>
+            </SignedOut>
+            <SignedIn><Images /></SignedIn>
         </main>
     );
 }
