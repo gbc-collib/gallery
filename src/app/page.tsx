c38d0cd450d10c29be5ee8b4a5d6bbd60441647a
Collin Stasiak <collinstasiak@gmail.com>
2024-05-30 08:04:13 -0400

    db added to ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,18 @@
-import Link from "next/link";
 import { db } from "~/server/db";
 
-const mockUrls = ['https://utfs.io/f/3c9cd41b-67e2-4c27-a9a9-981e1c26732b-pr278c.jpg', 'https://utfs.io/f/b3160f5f-0d7f-41ef-ae50-c8c25df0e533-hvwnay.jpg']
-const mockImages = mockUrls.map((url, index) => ({
-    id: index + 1,
-    url
-}));
+//Tell next not to cache because database will change
+export const dynamic = "force-dynamic";
+
 
 export default async function HomePage() {
-    const posts = await db.query.posts.findMany();
+    const images = await db.query.images.findMany({ orderBy: (model, { desc }) => desc(model.id), });
     return (
         <main>
             <div className="flex flex-wrap gap-4">
-                {
-                    posts.map((post) => (
-                        <div key={post.id}>{post.name}</div>)
-                    )
-                }
-                {mockImages.map((image) => (
-                    <div key={image.id} className="w-48 ">
+                {[...images, ...images, ...images].map((image) => (
+                    <div key={image.id} className="w-48 flex flex-col">
                         <img src={image.url} alt="image" />
+                        <div>{image.name}</div>
                     </div>
                 ))
                 }
